a41c9719a694e2e1a09d31b56f4f2868276e5efe
Chris Phillips <cp.phillips15@gmail.com>
2022-03-07 23:22:18 -0500

    added test for methods uniqe to Intern

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -10,3 +10,16 @@ test('create Intern Object', () => {
     expect(intern.school).toBe('school')
 });
 
+//test getSchool()
+test('get Interns school as an object', () => {
+    const intern = new Intern('TestIntern', 1, 'test@email.com', 'school');
+
+    expect(intern.getSchool()).toEqual('school');
+});
+
+//test getRole() 
+test('get interns name as an object', () => {
+    const intern = new Intern('TestIntern', 1, 'test@email.com', 'school');
+
+    expect(intern.getRole()).toEqual('Intern');
+});
