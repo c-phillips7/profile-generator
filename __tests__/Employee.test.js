a2b4790ca9d087863bdd548e16dfebca8c87c4ab
Chris Phillips <cp.phillips15@gmail.com>
2022-03-07 23:16:32 -0500

    added test to check nested getName() function from Employee class

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -7,4 +7,11 @@ test('create Employee Object', () => {
     expect(employee.name).toBe('TestEmployee');
     expect(employee.id).toBe(1);
     expect(employee.email).toBe('test@email.com');
-})
+});
+
+//test getName()
+test('get Employees name as an object', () => {
+    const employee = new Employee('TestEmployee', 1, 'test@email.com');
+
+    expect(employee.getName()).toEqual('TestEmployee');
+});
